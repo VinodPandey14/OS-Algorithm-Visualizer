f0611a963272b2e85c456de67ec2d086acf3031e
Vinod Pandey <itsvinod14@gmail.com>
2025-06-22 10:44:32 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from util import get_algorithm_function 
+from util import get_algorithm_function
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -11,10 +12,11 @@ def visualize():
     input_data = data.get("inputData")
     algo_type = data.get("type")
     algo = data.get("algo")
-    quantum = data.get("quantum", 4) 
+    quantum = data.get("quantum", 4)
 
-    result = get_algorithm_function(algo_type, algo, input_data, quantum)  
+    result = get_algorithm_function(algo_type, algo, input_data, quantum)
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
